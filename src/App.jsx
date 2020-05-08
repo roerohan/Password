2c356d9981b7a0cce4f032596c1eee7eb9a385a2
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-08 13:04:58 +0530

    feat: Use App.css and bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Home from './components/Home';
 import WaitingRoom from './components/WaitingRoom';
+
+import './assets/css/App.css';
 
 function App() {
   return (
