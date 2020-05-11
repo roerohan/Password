2c1034fd3a5f8a4138311fb1835b50e6220fd75d
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-11 15:19:13 +0530

    feat: Game.jsx basic structure established

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+} from 'react-bootstrap';
+import propTypes from 'prop-types';
 
-function Game() {
-  return <div>Hello!</div>;
+import Heading from './Heading';
+import Chat from './Chat';
+
+function Game(props) {
+  const {
+    username,
+    roomId,
+    sendMessage,
+    messageList,
+  } = props;
+
+  return (
+    <Container fluid className="lobby-container">
+      <Heading />
+      <Row className="mt-4">
+        <Col md />
+        <Col md className="text-center mb-4">
+          <Button type="submit" variant="success">Game</Button>
+          <div>
+            {username}
+            ,
+            {' '}
+            {roomId}
+
+          </div>
+        </Col>
+        <Col>
+          <Chat
+            sendMessage={sendMessage}
+            messageList={messageList}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
+
+Game.propTypes = {
+  username: propTypes.string.isRequired,
+  roomId: propTypes.string.isRequired,
+  sendMessage: propTypes.func.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  messageList: propTypes.array.isRequired,
+};
 
 export default Game;
