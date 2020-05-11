4950b14e9029a7304f6e0e131d81ec12dd2497d5
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-11 15:42:02 +0530

    feat: Prop validation and pass players to list

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -9,11 +9,13 @@ import propTypes from 'prop-types';
 
 import Heading from './Heading';
 import Chat from './Chat';
+import PlayerList from './PlayerList';
 
 function Game(props) {
   const {
     username,
     roomId,
+    players,
     sendMessage,
     messageList,
   } = props;
@@ -22,7 +24,11 @@ function Game(props) {
     <Container fluid className="lobby-container">
       <Heading />
       <Row className="mt-4">
-        <Col md />
+        <Col md>
+          <PlayerList
+            players={players}
+          />
+        </Col>
         <Col md className="text-center mb-4">
           <Button type="submit" variant="success">Game</Button>
           <div>
@@ -48,8 +54,16 @@ Game.propTypes = {
   username: propTypes.string.isRequired,
   roomId: propTypes.string.isRequired,
   sendMessage: propTypes.func.isRequired,
-  // eslint-disable-next-line react/forbid-prop-types
-  messageList: propTypes.array.isRequired,
+  messageList: propTypes.arrayOf(propTypes.shape({
+    message: propTypes.string,
+    username: propTypes.string,
+    time: propTypes.string,
+  })).isRequired,
+  players: propTypes.arrayOf(propTypes.shape({
+    username: propTypes.string,
+    points: propTypes.number,
+    _id: propTypes.string,
+  })).isRequired,
 };
 
 export default Game;
