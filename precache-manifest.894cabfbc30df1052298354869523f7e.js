8a0ed88367ed084deb59036a0d6929103abdb869
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-16 22:38:52 +0530

    Updates

---
+++ precache-manifest.894cabfbc30df1052298354869523f7e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4ad68f332494ba129560426aa8cfde",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13b0ab76675e462bdfb4",
+    "url": "./static/css/2.00212987.chunk.css"
+  },
+  {
+    "revision": "4600eeddfd4d2db5e7a3",
+    "url": "./static/css/main.3261d182.chunk.css"
+  },
+  {
+    "revision": "13b0ab76675e462bdfb4",
+    "url": "./static/js/2.4707316c.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "./static/js/2.4707316c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4600eeddfd4d2db5e7a3",
+    "url": "./static/js/main.c087fe56.chunk.js"
+  },
+  {
+    "revision": "5a6ab7208fc691009b48",
+    "url": "./static/js/runtime-main.e9973afd.js"
+  },
+  {
+    "revision": "52d0b6af3cc627b56e9435b6719e829e",
+    "url": "./static/media/background.52d0b6af.png"
+  },
+  {
+    "revision": "7d7ce87981d12e5ca776e4ffe8616504",
+    "url": "./static/media/title.7d7ce879.png"
+  }
+]);
